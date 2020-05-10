d5220e22e314b7004c9e94459cdda18bad533c09
Jason Coleman <jayemcee121@gmail.com>
2020-05-09 21:32:08 -0400

    fixed seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers_db (burger_name, devoured) VALUES ("Big Mac", false);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ("Whopper", false);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ("Baconator", false);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ("Bacon-Double Cheese Burger", false);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ("SantaFe Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon-Double Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("SantaFe Burger", false);
